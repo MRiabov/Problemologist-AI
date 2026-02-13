4f14ad82de88b282dc7c1cf2890f2341e1e961e1
MRiabov <maksymriabov2004@gmail.com>
2026-02-13 17:15:57 +0000

    Add settings page and theme support

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,21 +2,26 @@ import { Routes, Route } from 'react-router-dom';
 import AppLayout from './components/layout/AppLayout';
 import EngineerWorkspace from './pages/EngineerWorkspace';
 import BenchmarkGeneration from './pages/BenchmarkGeneration';
+import Settings from './pages/Settings';
 import { EpisodeProvider } from './context/EpisodeContext';
 import { ConnectionProvider } from './context/ConnectionContext';
+import { ThemeProvider } from './context/ThemeContext';
 
 function App() {
   return (
-    <ConnectionProvider>
-      <EpisodeProvider>
-        <Routes>
-          <Route path="/" element={<AppLayout />}>
-            <Route index element={<EngineerWorkspace />} />
-            <Route path="benchmark" element={<BenchmarkGeneration />} />
-          </Route>
-        </Routes>
-      </EpisodeProvider>
-    </ConnectionProvider>
+    <ThemeProvider>
+      <ConnectionProvider>
+        <EpisodeProvider>
+          <Routes>
+            <Route path="/" element={<AppLayout />}>
+              <Route index element={<EngineerWorkspace />} />
+              <Route path="benchmark" element={<BenchmarkGeneration />} />
+              <Route path="settings" element={<Settings />} />
+            </Route>
+          </Routes>
+        </EpisodeProvider>
+      </ConnectionProvider>
+    </ThemeProvider>
   );
 }
 
