de54aa6ad24367c1841d86cd6ecdb5e16da4229d
MRiabov <maksymriabov2004@gmail.com>
2026-02-13 10:05:38 +0000

    (debug) Fix Langfuse trace formatting to prevent server errors

--- controller/observability/langfuse.py
+++ controller/observability/langfuse.py
@@ -213,7 +213,7 @@ def get_langfuse_client() -> Langfuse | None:
     """
     public_key = os.getenv("LANGFUSE_PUBLIC_KEY")
     secret_key = os.getenv("LANGFUSE_SECRET_KEY")
-    host = os.getenv("LANGFUSE_HOST", "http://localhost:3000")
+    host = os.getenv("LANGFUSE_HOST", "http://localhost:13000")
 
     if public_key and secret_key:
         return Langfuse(public_key=public_key, secret_key=secret_key, host=host)
