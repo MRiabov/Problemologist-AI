ecd045c2b2a5c48a02aa9bde041b6c4e71e1864f
MRiabov <maksymriabov2004@gmail.com>
2026-02-13 17:15:45 +0000

    (refactor) Remove unused Layout component

--- frontend/src/components/Layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from 'react-router-dom';
-
-export default function Layout() {
-  return (
-    <div className="h-screen w-full bg-background-dark text-white font-display">
-      <Outlet />
-    </div>
-  );
-}
