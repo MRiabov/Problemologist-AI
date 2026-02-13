c658193a19f230b0f77ee4029f82876c3ee67f45
MRiabov <maksymriabov2004@gmail.com>
2026-02-13 19:49:14 +0000

    (debug) debugging app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,25 +3,16 @@ import AppLayout from './components/layout/AppLayout';
 import EngineerWorkspace from './pages/EngineerWorkspace';
 import BenchmarkGeneration from './pages/BenchmarkGeneration';
 import Settings from './pages/Settings';
-import { EpisodeProvider } from './context/EpisodeContext';
-import { ConnectionProvider } from './context/ConnectionContext';
-import { ThemeProvider } from './context/ThemeContext';
 
 function App() {
   return (
-    <ThemeProvider>
-      <ConnectionProvider>
-        <EpisodeProvider>
-          <Routes>
-            <Route path="/" element={<AppLayout />}>
-              <Route index element={<EngineerWorkspace />} />
-              <Route path="benchmark" element={<BenchmarkGeneration />} />
-              <Route path="settings" element={<Settings />} />
-            </Route>
-          </Routes>
-        </EpisodeProvider>
-      </ConnectionProvider>
-    </ThemeProvider>
+    <Routes>
+      <Route path="/" element={<AppLayout />}>
+        <Route index element={<EngineerWorkspace />} />
+        <Route path="benchmark" element={<BenchmarkGeneration />} />
+        <Route path="settings" element={<Settings />} />
+      </Route>
+    </Routes>
   );
 }
 
